3fc4ea0c57ccea1648f6032bbb73d6eda720af3f
Kelvin Souza <kelvinsouza2017@hotmail.com>
2023-11-11 13:53:08 -0300

    Subindo estrutura base, desenvolvimento e produção

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+export function App() {
+  return (
+    <h1>
+      FIAP
+      <br />
+      Digite o seu CEP
+    </h1>
+  );
+}
+
+export default App;
